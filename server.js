85f190f063a870e6a4a82f5693dd46ab124fb3ef
Nadina Gerlach <nadina@nadina.la>
2018-04-03 13:55:08 -0700

    push fun

--- server.js
+++ server.js
@@ -4,4 +4,11 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist/'));
 
+// views is directory for all template files
+app.get('/', function(request, response) {
+    response.sendFile(__dirname + '/dist/index.html');
+  });
+
 app.listen(process.env.PORT || 5000);
+
+  
