6da59ae2eb34b55b1c38a51d5ecc2929db31195d
Nadina Gerlach <nadina@nadina.la>
2018-04-05 13:50:31 -0700

    push menu

--- app/scripts/components/Menu.jsx
+++ app/scripts/components/Menu.jsx
@@ -14,9 +14,21 @@ export class BurgerMenu extends React.Component {
   render() {
     return (
       <Menu right >
-        <a id="home" className="menu-item" href="/">Home</a>
-        <a id="about" className="menu-item" href="/about">About</a>
-        <a id="contact" className="menu-item" href="/contact">Contact</a>
+        <a id="home" className="menu-item" href="/">Our Story</a>
+        <a id="home" className="menu-item" href="/">Our Cigars</a>
+        <a id="home" className="menu-item" href="/">Find Your Blend</a>
+        <a id="home" className="menu-item" href="/">Locations</a>
+        <a id="home" className="menu-item" href="/">Shop</a>
+        <br />
+        <a id="home" className="menu-item" href="/">Our Story</a>
+        <i className="i-instagram" />
+        <i className="i-facebook" />
+        <i className="i-twitter" />
+        <br />
+        <a id="home" className="menu-item" href="/">Contact</a>
+        <a id="home" className="menu-item" href="/">FAQ</a>
+        <a id="home" className="menu-item" href="/">Terms</a>
+        <a id="home" className="menu-item" href="/">Privacy</a>
         <a onClick={this.showSettings} className="menu-item--small" href="">Settings</a>
       </Menu>
     );
