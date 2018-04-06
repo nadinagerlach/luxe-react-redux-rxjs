cb19561e6f1af5e3bfb417b4c7ba351927615a6e
Nadina Gerlach <nadina@nadina.la>
2018-04-05 17:04:31 -0700

    sync db tables (TBD) and start server on config.port. catch errors and console status

---
+++ api/src/setup/startServer.js
@@ -0,0 +1,26 @@
+import models from '..models/index';
+import config from '../config/config.json';
+
+export default function(server) {
+  console.info('SETUP - Syncing db tables...');
+
+  models.sequelize.sync({})
+    .then(() => {
+      console.info('INFO - Db sync complete.');
+
+      console.info('SETUP - Starting server...');
+
+      server.listen(config.port, (error) => {
+        if (error) {
+          console.error('ERROR - Unable to start server.');
+        } else {
+          console.info(`INFO - Server started on port ${config.port}.`);
+        }
+      });
+    })
+    .catch(() => {
+      console.error('ERROR - Unable to sync database.');
+      console.error('ERROR - Server not started.');
+    });
+}
+
