1b258cca8383d92304c3d27dc0d7fb31c765d8a9
Nadina Gerlach <nadina@nadina.la>
2018-04-05 17:17:21 -0700

    load express modules, request body parser, request body cookie parser, http logger

---
+++ api/src/setup/loadModules.js
@@ -0,0 +1,17 @@
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+
+export default function (server) {
+  console.info('SETUP - Loading modules...');
+
+  server.use(cors());
+
+  server.use(bodyParser.json());
+  server.use(bodyParser.urlencoded({extended: false}));
+
+  server.use(cookieParser());
+
+  server.use(morgan('tiny'));
+}
