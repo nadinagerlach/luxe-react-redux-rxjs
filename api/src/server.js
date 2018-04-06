6f5cc62d315fe23778a1e9e564c84857a2a25019
Nadina Gerlach <nadina@nadina.la>
2018-04-05 17:16:06 -0700

    stub server setup remaining items

--- api/src/server.js
+++ api/src/server.js
@@ -1,3 +1,11 @@
 import express from 'express';
 
+//LoadMOdules
+//GraphQL
+import setupStartServer from './setup/startServer';
+
 const server = express();
+//LoadMOdules
+//GraphQL
+
+setupStartServer(server);
